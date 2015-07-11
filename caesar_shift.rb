fc02613709920ba0c41b1618e657257deea0c747
Anthony Larios <simpsonsruleA52@gmail.com>
2015-07-11 01:27:11 -0700

    add comments and modify dialogue for entering shift number

--- caesar_shift.rb
+++ caesar_shift.rb
@@ -1,14 +1,18 @@
 def caesar_cipher(string, cipher)
 	characters = string.split("")
+	#we only need the remainder if the number entered is too large
 	cipher %= 26
 
+	#initialize the array and text output
 	text_shift = ""
 	characters_shift = []
 
 	characters.each do |character|
+		#this block finds and modifies lower/uppercase letters
 		character.scan(/[a-z]|[A-Z]/) do |letter|
 			cipher_letter = letter.ord + cipher
 
+			#this block helps wrap letters from z to a by looking at their ASCII value
 			if (letter.ord < 91 && cipher_letter > 90) || (cipher_letter > 122)
 				character = (cipher_letter - 26).chr
 			else
@@ -26,10 +30,11 @@ end
 puts "Say anything!"
 text = gets.chomp
 
-number = -1
-until number >= 0
-	puts "Enter a number greater than or equal to 0"
-	number = gets.chomp.to_i
-end
+number = 0
+puts "Enter an integer"
+#a negative integer is okay since it shifts letters in the 'opposite' direction
+#example: -1 % 26 = 25, which still turns 'a' into 'z'
+puts "We round down and a non-number produces a 0..."
+number = gets.chomp.to_i
 
 caesar_cipher(text, number)
